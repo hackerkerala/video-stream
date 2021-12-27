6a37e4f796015471832800eb48a78dcf8d74633a
hackerkerala <85721811+hackerkerala@users.noreply.github.com>
2021-12-27 18:40:11 +0530

    Update sysinfo.py

--- program/sysinfo.py
+++ program/sysinfo.py
@@ -34,7 +34,7 @@ async def give_sysinfo(client, message):
     psutil.disk_io_counters()
     disk = f"{humanbytes(du.used)} / {humanbytes(du.total)} " f"({du.percent}%)"
     cpu_len = len(psutil.Process().cpu_affinity())
- await message.reply_text(
+    await message.reply_text(
         f"""🖥 **System Information**
     
 **PlatForm :** `{splatform}`
