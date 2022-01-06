2ab9554c722332d653e8cad3d2d3bef7cc9be7ae
ALBY <68139513+ALBINPRAVEEN@users.noreply.github.com>
2022-01-06 21:09:59 +0530

    Create __init__.py

---
+++ Music/__init__.py
@@ -0,0 +1,14 @@
+
+def all_info(app, client):
+    global BOT_ID, BOT_NAME, BOT_USERNAME
+    getme = app.get_me()
+    getme1 = client.get_me()
+    BOT_ID = getme.id
+    ASSID = getme1.id
+    if getme.last_name:
+        BOT_NAME = getme.first_name + " " + getme.last_name
+    else:
+        BOT_NAME = getme.first_name
+    BOT_USERNAME = getme.username
+
+    
